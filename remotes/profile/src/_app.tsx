fbc5df0787dc76ef0ca3c0d8ccdce0cfbb9c030a
Chris Fryer <cfryerdev@gmail.com>
2023-01-18 10:47:22 -0700

    Updates for MF

--- remotes/profile/src/_app.tsx
+++ remotes/profile/src/_app.tsx
@@ -1,17 +1,16 @@
 import React, { useContext } from "react";
-import Layout from "./components/layout";
 
-const App = () => {
+const Profile = () => {
 	return (
-		<Layout>
+		<>
 			<ol className="breadcrumb">
 				<li className="breadcrumb-item">Home</li>
 				<li className="breadcrumb-item active">Profile</li>
 			</ol>
 			<h2>Remote App - Profile</h2>
 			<p>This is the profile remote application.</p>
-		</Layout>
+		</>
 	);
 }
 
-export default App;
+export default Profile;
